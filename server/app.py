7ffac0ab727cd2755697e38912e8373d3816e5bb
zeid <zeidhassan4@gmail.com>
2024-04-07 11:25:47 +0300

    Done

--- server/app.py
+++ server/app.py
@@ -20,19 +20,54 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    """Return a list of all bakeries."""
+    bakeries = Bakery.query.all()
+    bakery_list = []
+    for b in bakeries:
+        bakery_list.append(b.to_dict())
+
+    response = make_response(
+        bakery_list,
+        200
+    )
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    """Return the details of a single bakery by its ID."""
+    target_bakery = Bakery.query.get(id)
+    if target_bakery:
+        return make_response(target_bakery.to_dict(), 200)
+    else:
+        return make_response({"Error":"The Bakery with that ID was not found"}, 404)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    my_baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+  
+    baked_list = []
+
+    for baked_good in my_baked_goods:
+        baked_list.append(baked_good.to_dict())
+
+    response = make_response(
+        baked_list,
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    
+    if baked_good:
+        return make_response(
+            baked_good.to_dict(),
+            200
+        )
+    else:
+        return make_response({"Error":"The baked food is not found"}, 404)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
